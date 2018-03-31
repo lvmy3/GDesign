be02de1ae2cee5d7ac62c60c1a9fb750cb09ed17
Dustin <854504884@qq.com>
2018-03-31 10:08:14 +0800

    original version

---
+++ Test2/index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>吕铭洋的博客</title>
+        <style type="text/css">
+        	.bloglist {
+				width:730px;
+                margin-bottom:10px;
+                height:1000px;
+			}
+			.blglist {
+				border-bottom: #474645 1px solid; 
+				width: 700px; 
+				overflow: hidden; 
+				font-size: 14px; 
+				margin: 0 0 0 15px; 
+				font-family: "微软雅黑"; 
+				display: block; 
+			}
+			.blglist p {
+				background: #474645; 
+				width: 180px; 
+				height: 25px; 
+				margin: 15px 0 0 0px; 
+				line-height: 25px; 
+				color: #fff; 
+				text-align: center; 
+				box-shadow: #999 4px 5px 1px;
+				float:left;
+			}
+			.blglist span {
+				float:right;
+				text-align:center;
+				margin:15px 0 0 0;
+			}
+			.blg {
+				position:relative;
+			}
+			.bloglist h3 {
+				margin: 20px 0 15px 30px; 
+				color: #333;
+			}
+			.bloglist img { 
+				float:left;
+				border: 0; 
+				width: 175px; 
+				margin-left:15px;
+				box-shadow:0 0 30px 10px rgba(255,255,255,.7) inset;
+			}
+			.abody { 
+				float: left; 
+				width: 450px; 
+				height:120px;
+				margin: 0px 0px 0 15px; 
+				line-height: 20px; 
+			}
+			.abody p {
+				margin:0;
+				padding:0;
+				color: #838383;
+				width:500px;
+			}
+			.dateview { 
+				width: 695px; 
+				overflow: hidden;
+				clear: both; 
+				margin: 10px 0 0 0; 
+				display: inline-block;
+				background: #f6f6f6;
+				line-height: 26px;
+			    height: 26px;
+			    color: #838383;
+			    padding-left: 25px;
+			    vertical-align:bottom;
+			}
+			.readmore {
+				background: red; 
+				color: #fff; padding: 5px 10px; 
+				position:absolute;
+				left:630px;
+				top:40px;
+				margin: 70px 0 0 0;
+				text-decoration:none;
+			}
+			.anniu {
+				width:70px;
+				height:22px;
+				text-align:center;
+				margin:10px auto;
+				background:rgb(55,129,240);
+				border-radius:4px;
+			}
+			.anniu a {
+				text-decoration:none;
+				vertical-align:middle;
+				color:white;
+			}
+			body {font: 12px "宋体", Arial, Helvetica, sans-serif;background: #f5f5f5;margin:0;padding:0;background-size:100%;}
+			.ibody {background:url(images/bg.png) repeat-y;box-shadow: -2px -1px 5px #666;margin:0 auto;padding:0;width:1000px;background-size:100%;}
+			header {position:relative;}
+			#motto {float:right;width:16%;margin-top:18px;background:#000}
+			#motto p{font-size:18px;color:white;font-weight:20;font-family:Arial;margin:15;margin-bottom:0;padding:0;}
+			#hat {position:absolute;left:712px;top:8px;width:130px;}
+			h1 {font-size: 22px;font-family: Arial; margin-left: 80px;margin-top:0;}
+			header h2 {font-size: 13px;font-family: Arial; margin-left:100px; color:#8e8d8d;animation:animate 3s linear 1s forwards;
+			-webkit-animation:animate 3s linear 1s forwards;-moz-animation:animate 3s linear 1s forwards;}
+			@keyframes animate{
+			    0%{transform:translate(0px);opacity:0}
+			    100%{transform:translate(30px);opacity:1;}
+			}
+			nav {background: #434343; width:730px;line-height:48px;}
+			nav div{padding-left:100px;}
+			nav a{color:white; font-size:1.5em;padding:10px;text-decoration:none;border-radius:4px;transition:all 0.1s linear;
+			     text-shadow: rgba(0,0,0,0.9) 0px 1px 2px;}
+			nav a:hover{color:yellow;box-shadow: rgba(0,0,0,0.5) 0px 2px 5px, inset rgba(255,255,255,0.25) 0px 1px 0px, inset rgba(0,0,0,0.25) 0px 0px 0px, inset rgba(255,255,255,0.03) 0px 20px 0px, inset rgba(0,0,0,0.15) 0px -20px 20px, inset rgba(255,255,255,0.05) 0px 20px 20px;}
+			#logo {height:100px;border-radius:50px;background:rgba(0,0,0,1);position:absolute;width:100px;left:-10px;top:50px;}
+			#logo img {transition-property:all;transition-duration:0.5s;}
+			#logo:hover img{transform:scale(1.5,1.5);}
+			article {width:73%;float:left;}
+			.photo {background:url(images/Love.png) no-repeat center;margin:10px;width:710px;height:300px;}
+			aside { width: 27%;background:#000;float: right;margin-bottom:20px;}
+			#avatar {text-align:center}
+			#avatar img{width:150px;height:150px;overflow:hidden;border-radius:100px;margin-top:20px;} 
+			#avatar~div {height:10px;color:white;margin:10px;font-size:15px;margin-left:50px;}
+			#avatar~div img {width:20px;margin:0;padding:0;vertical-align:middle;}
+			#avatar~div span{vertical-align:middle;}
+            .clr {clear:both;}
+            .frilink {background:#000}
+			.frilink h3 {position: relative; width:150px; background:yellow; color:black; left:-50px; text-align:center;margin-top:50px;border-top-right-radius:15px;border-bottom-right-radius:15px;}
+			.frilink ul {position:relative;left:-50px;width:200px;}
+        </style>
+
+    </head>
+    <body> 
+        <div class="ibody">
+        <header>
+            <div id="motto"><p>Fighting for Free</p><p>为自由而战</p></div>
+            <h1>没文艺的少年</h1>
+            <h2>春风十里不如你...</h2>
+            <div id="logo"><img src="images/Logo.png"></img></div>
+            <img id="hat" src="images/Hat.png"/>
+            <nav id="topnav">
+                <div>
+                    <a href="doindex">首页</a><a href="info.jsp">个人信息</a><a href="getblglist">博客</a>
+                </div>
+            </nav>
+        </header>
+        <article>
+            <div class="photo"></div>
+            <h3 class="blglist">
+            	<p>文章推荐</p>
+            </h3>
+            <div class="bloglist">
+                
+            </div>
+        </article>
+        <aside>
+            <div id="avatar"><img src="images/lufei.jpg"></img></div>
+            <div><img src="images/name.png"/><span>&nbsp&nbsp昵称 : 没文艺的少年</span></div>
+            <div><img src="images/phone.png"/><span>&nbsp&nbsp电话 : 18826072060</span></div>
+            <div><img src="images/qq.png"/><span>&nbsp&nbspQQ : 854504884</span></div>
+            <div><img src="images/email.png"/><span>&nbsp&nbsp邮箱 : 854504884@qq.com</span></div>
+            <div class="frilink">
+            	<h3>CSDN博客推荐</h3>
+            	<ul>
+            		<li><a href="http://blog.csdn.net/lvmy3/article/details/72627399">MWeb 更新CSDN博客</a></li>
+            		<li><a href="http://blog.csdn.net/lvmy3/article/details/54969070">CentOS 5.5下编译安装新内核</a></li>
+            		<li><a href="http://blog.csdn.net/lvmy3/article/details/54891451">NFS及pNFS简介</a></li>
+            		<li><a href="http://blog.csdn.net/lvmy3/article/details/54891389">VMware下CentOS7网络重启出错解决方案</a></li>
+            	</ul>
+            </div>
+        </aside>
+        <div class="clr"></div>
+        </div>  
+    </body>
+</html>
+
+
+
